ceb086a60383316e8d0e309d83174f31280b3182
Kevin Cowdrey <kevin@gendac.co.za>
2020-01-17 09:57:24 +0200

    Add project files.

---
+++ DomainDrivenDesign.Logic/Money.cs
@@ -0,0 +1,40 @@
+﻿namespace DomainDrivenDesign.Logic
+{
+    public sealed class Money
+    {
+        public int FiftyCentCount { get; private set; }
+        public int OneRandCount { get; private set; }
+        public int TwoRandCount { get; private set; }
+        public int FiveRandCount { get; private set; }
+        public int TenRandCount { get; private set; }
+        public int TwentyRandCount { get; private set; }
+        public int FiftyRandCount { get; private set; }
+
+        public Money(int fiftyCents, int oneRands, int twoRands, int fiveRands, int tenRands, int twentyRands, int fiftyRands)
+        {
+            FiftyCentCount = fiftyCents;
+            OneRandCount = oneRands;
+            TwoRandCount = twoRands;
+            FiveRandCount = fiveRands;
+            TenRandCount = tenRands;
+            TwentyRandCount = twentyRands;
+            FiftyRandCount = fiftyRands;
+        }
+
+        //Operator to add two money objects together.
+        public static Money operator +(Money left, Money right)
+        {
+            Money sum = new Money
+                (
+                    left.FiftyCentCount + right.FiftyCentCount,
+                    left.OneRandCount + right.OneRandCount,
+                    left.TwoRandCount + right.TwoRandCount,
+                    left.FiveRandCount + right.FiveRandCount,
+                    left.TenRandCount + right.TenRandCount,
+                    left.TwentyRandCount + right.TwentyRandCount,
+                    left.FiftyRandCount + right.FiftyRandCount
+                );
+            return sum;
+        }
+    }
+}
---
+++ DomainDrivenDesign.Logic/VendingMachine.cs
@@ -0,0 +1,25 @@
+﻿namespace DomainDrivenDesign.Logic
+{
+    public sealed class VendingMachine
+    {
+        public Money MoneyInMachine;
+        public Money MoneyInTransaction;
+
+        public void InsertMoney(Money moneyInserted)
+        {
+            MoneyInTransaction += moneyInserted;
+        }
+
+        public void ReturnMoney()
+        {
+            //MoneyInTransaction = 0;
+        }
+
+        public void BuyAnItem()
+        {
+            MoneyInMachine += MoneyInTransaction;
+            //MoneyInTransaction = 0;
+        }
+
+    }
+}
